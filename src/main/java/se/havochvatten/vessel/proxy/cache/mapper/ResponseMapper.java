419808754a67a4b65f7f1570b7c70a3e381efc5b
peerik <peter.eriksson@havochvatten.se>
2021-03-17 14:22:58 +0100

    Add information from VesselEuFormat

--- src/main/java/se/havochvatten/vessel/proxy/cache/mapper/ResponseMapper.java
+++ src/main/java/se/havochvatten/vessel/proxy/cache/mapper/ResponseMapper.java
@@ -23,6 +23,14 @@ import org.apache.commons.lang3.math.NumberUtils;
 import se.havochvatten.service.client.vesselcompws.v2_0.orgpers.OrganisationType;
 import se.havochvatten.service.client.vesselcompws.v2_0.orgpers.RolePersonType;
 import se.havochvatten.service.client.vesselcompws.v2_0.vessel.OwnerType;
+import se.havochvatten.service.client.vesselws.v2_1.vessel.AddressType;
+import se.havochvatten.service.client.vesselws.v2_1.vessel.ConstructionType;
+import se.havochvatten.service.client.vesselws.v2_1.vessel.ContactDetailsType;
+import se.havochvatten.service.client.vesselws.v2_1.vessel.DimensionType;
+import se.havochvatten.service.client.vesselws.v2_1.vessel.EngineType;
+import se.havochvatten.service.client.vesselws.v2_1.vessel.EquipmentType;
+import se.havochvatten.service.client.vesselws.v2_1.vessel.IdentificationType;
+import se.havochvatten.service.client.vesselws.v2_1.vessel.RegistrationType;
 import se.havochvatten.service.client.vesselws.v2_1.vessel.Vessel;
 import se.havochvatten.service.client.vesselws.v2_1.vessel.VesselEuFormatType;
 
@@ -30,7 +38,7 @@ public class ResponseMapper {
     
     private static final List<String> OUT_EVENT_CODES = Arrays.asList("DES", "EXP", "RET");
 
-    private ResponseMapper() {};
+    private ResponseMapper() {}
 
     public static AssetDTO mapToAsset(Vessel vessel) {
         AssetDTO asset = new AssetDTO();
@@ -59,25 +67,14 @@ public class ResponseMapper {
         return asset;
     }
     
-    public static void enrichAssetWithEuFormatInformation(AssetDTO asset, VesselEuFormatType vesselEu) {
+    public static void enrichWithEuFormatInformation(AssetDTO asset, List<ContactInfo> contacts, VesselEuFormatType vesselEu) {
         if (vesselEu != null) {
-            if (vesselEu.getIdentification() != null && vesselEu.getIdentification().getMmsi() != null) {
-                asset.setMmsi(vesselEu.getIdentification().getMmsi().toString());
-            }
-            if(vesselEu.getRegistration().getPortName() != null) {
-                if(vesselEu.getRegistration().getPortCode() != null) {
-                    asset.setPortOfRegistration(vesselEu.getRegistration().getPortCode() + " " + 
-                            vesselEu.getRegistration().getPortName());  
-                }else {
-                    asset.setPortOfRegistration(vesselEu.getRegistration().getPortName());   
-                }
-            }
-            if (vesselEu.getConstruction() != null) {
-                if (vesselEu.getConstruction().getYearOfConstruction() != null) {
-                    asset.setConstructionYear(vesselEu.getConstruction().getYearOfConstruction().toString());
-                }
-                asset.setConstructionPlace(vesselEu.getConstruction().getPlaceOfConstruction());
-            }
+            enrichIdentificationType(asset, vesselEu);
+            enrichRegistrationType(asset, vesselEu);
+            enrichEquipmentType(asset, vesselEu);
+            enrichDimensionType(asset, vesselEu);
+            enrichEngineType(asset, vesselEu);
+            enrichConstructionType(asset, vesselEu);
             if (vesselEu.getEvent() != null) {
                 String eventCode = vesselEu.getEvent().getEventCode();
                 asset.setEventCode(eventCode);
@@ -85,9 +82,145 @@ public class ResponseMapper {
                     asset.setActive(false);
                 }
             }
+            if (contacts.isEmpty() && !vesselEu.getOwner().isEmpty()) {
+                enrichOwner(contacts, vesselEu);
+            }
         }
     }
-    
+
+    private static void enrichIdentificationType(AssetDTO asset, VesselEuFormatType vesselEu) {
+        IdentificationType identification = vesselEu.getIdentification();
+        if (identification != null) {
+            if (vesselEu.getIdentification().getMmsi() != null) {
+                asset.setMmsi(vesselEu.getIdentification().getMmsi().toString());
+            }
+            if (vesselEu.getIdentification().getImo() != null) {
+                asset.setImo(vesselEu.getIdentification().getImo());
+            }
+            if (vesselEu.getIdentification().getUvi() != null) {
+                asset.setUvi(vesselEu.getIdentification().getUvi());
+            }
+        }
+    }
+
+    private static void enrichRegistrationType(AssetDTO asset, VesselEuFormatType vesselEu) {
+        RegistrationType registration = vesselEu.getRegistration();
+        if (registration != null) {
+            if (asset.getPortOfRegistration() == null) {
+                if (vesselEu.getRegistration().getPortName() != null) {
+                    asset.setPortOfRegistration(vesselEu.getRegistration().getPortCode() + " " + vesselEu.getRegistration().getPortName());
+                } else {
+                    asset.setPortOfRegistration(vesselEu.getRegistration().getPortCode());
+                }
+            }
+            if (registration.getDateOfEIS() != null) {
+                asset.setVesselDateOfEntry(registration.getDateOfEIS().toGregorianCalendar().toInstant());
+            }
+            asset.setSegment(registration.getSegment());
+            asset.setTypeOfExport(registration.getExportType());
+            asset.setCountryOfImportOrExport(registration.getImportOrExportCountry());
+            asset.setSegmentOfAdministrativeDecision(registration.getAdministrativeSegment());
+            asset.setPublicAid(registration.getPublicAID());
+        }
+    }
+
+    private static void enrichEquipmentType(AssetDTO asset, VesselEuFormatType vesselEu) {
+        EquipmentType equipment = vesselEu.getEquipment();
+        if (equipment != null) {
+            asset.setVmsIndicator(equipment.isVmsIndicator());
+            asset.setErsIndicator(equipment.isErsIndicator());
+            asset.setAisIndicator(equipment.isAisIndicator());
+            asset.setMainFishingGearCode(equipment.getMainFishingGear());
+            asset.setSubFishingGearCode(equipment.getSubsidiaryFishingGear());
+        }
+    }
+
+    private static void enrichDimensionType(AssetDTO asset, VesselEuFormatType vesselEu) {
+        DimensionType dimension = vesselEu.getDimension();
+        if (dimension != null) {
+            if (dimension.getLoa() != null) {
+                asset.setLengthOverAll(dimension.getLoa().doubleValue());
+            }
+            if (dimension.getTonnageGT() != null) {
+                asset.setGrossTonnage(dimension.getTonnageGT().doubleValue());
+            }
+            if (dimension.getTonnageOther() != null) {
+                asset.setOtherTonnage(dimension.getTonnageOther().doubleValue());
+            }
+            if (dimension.getLengthBetweenPerpendiculars() != null) {
+                asset.setLengthBetweenPerpendiculars(dimension.getLengthBetweenPerpendiculars().doubleValue());
+            }
+        }
+    }
+
+    private static void enrichEngineType(AssetDTO asset, VesselEuFormatType vesselEu) {
+        EngineType engine = vesselEu.getEngine();
+        if (engine != null) {
+            if (engine.getPowerOfMainEngine() != null) {
+                asset.setPowerOfMainEngine(engine.getPowerOfMainEngine().doubleValue());
+            }
+            if (engine.getPowerOfAuxEngine() != null) {
+                asset.setPowerOfAuxEngine(engine.getPowerOfAuxEngine().doubleValue());
+            }
+        }
+    }
+
+    private static void enrichConstructionType(AssetDTO asset, VesselEuFormatType vesselEu) {
+        ConstructionType construction = vesselEu.getConstruction();
+        if (construction != null) {
+            if (construction.getYearOfConstruction() != null) {
+                asset.setConstructionYear(construction.getYearOfConstruction().toString());
+            }
+            asset.setConstructionPlace(construction.getPlaceOfConstruction());
+            asset.setHullMaterial(construction.getHull());
+        }
+    }
+
+    private static void enrichOwner(List<ContactInfo> contacts, VesselEuFormatType vesselEu) {
+        for (VesselEuFormatType.Owner owner : vesselEu.getOwner()) {
+            if (owner.getPerson() != null) {
+                ContactInfo contactInfo = new ContactInfo();
+                contactInfo.setType("Person");
+                if (owner.getPerson().getGivenName() != null) {
+                    contactInfo.setName(owner.getPerson().getGivenName() + owner.getPerson().getFamilyName());
+                } else {
+                    contactInfo.setName(owner.getPerson().getFamilyName());
+                }
+                AddressType address = owner.getPerson().getAddress();
+                ContactDetailsType contactDetails = owner.getPerson().getContactDetails();
+                mapAddressType(contactInfo, address);
+                mapContactDetailsType(contactInfo, contactDetails);
+                contacts.add(contactInfo);
+            }
+            if (owner.getOrganisation() != null) {
+                ContactInfo contactInfo = new ContactInfo();
+                contactInfo.setType("Organization");
+                contactInfo.setName(owner.getOrganisation().getName());
+                AddressType address = owner.getOrganisation().getAddress();
+                ContactDetailsType contactDetails = owner.getOrganisation().getContactDetails();
+                mapAddressType(contactInfo, address);
+                mapContactDetailsType(contactInfo, contactDetails);
+                contacts.add(contactInfo);
+            }
+        }
+    }
+
+    private static void mapAddressType(ContactInfo contactInfo, AddressType address) {
+        if (address != null) {
+            contactInfo.setStreetName(address.getStreet());
+            contactInfo.setPostalArea(address.getPostCode());
+            contactInfo.setCityName(address.getCity());
+            contactInfo.setCountry(address.getCountry());
+        }
+    }
+
+    private static void mapContactDetailsType(ContactInfo contactInfo, ContactDetailsType contactDetails) {
+        if (contactDetails != null) {
+            contactInfo.setEmail(contactDetails.getEmail());
+            contactInfo.setPhoneNumber(contactDetails.getPhone());
+        }
+    }
+
     public static void enrichWithOrganisation(AssetDTO asset, List<OwnerType> owners) {
         for (OwnerType owner : owners) {
             OrganisationType organisation = owner.getOrganisation();
